8c0cf973069cca6016db7321e7574cb89a0bcbeb
Mauville <4991127@outlook.com>
2020-07-28 22:19:32 -0500

    Created PersonasSite.js

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,12 +1,15 @@
 import {ThemeProvider, CSSReset, Text, ColorModeProvider, Flex} from "@chakra-ui/core"
 import React from 'react';
 import BlogEntry from "./Blog/Entry";
+import Persona from "./components/Persona";
+import PersonasSite from "./Personas/PersonasSite";
 
 function LandingPage() {
     return (
         <div>
             <ThemeProvider>
-                <BlogEntry/>
+                <PersonasSite/>
+
             </ThemeProvider>
         </div>
     );
---
+++ src/components/Persona.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {Icon, IconButton, Image, Text} from "@chakra-ui/core";
+import Flex from "@chakra-ui/core/dist/Flex";
+import Box from "@chakra-ui/core/dist/Box";
+
+let persona = () => {
+    return (
+        <div>
+            <Box width="15em" maxHeight="25em">
+                <Flex textAlign="center" flexDirection="column">
+                    <Image src=" https://i.imgur.com/rLFk5nd.jpg" maxWidth="100%" maxHeight="40%"/>
+                    <Text fontSize="18px ">Rogelio Bustamante</Text>
+                    <Text paddingBottom="1em " color="grey">PhD. Computer Science</Text>
+                    <Flex justifyContent="space-around">
+                        <IconButton aria-label={"phone"} icon="phone"/>
+                        <IconButton aria-label={"mail"} icon="mail"/>
+                        <IconButton aria-label={"researchGate"} icon="search-2"/>
+                    </Flex>
+                </Flex>
+            </Box>
+        </div>
+    )
+}
+
+export default persona;
