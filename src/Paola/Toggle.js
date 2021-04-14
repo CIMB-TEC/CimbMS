4ddc6423e4c8da0ea5c5afac916822df06bf66dd
einar <einarlop0@gmail.com>
2021-04-13 21:40:48 -0500

    Example of components (Cardss)

--- src/Paola/Toggle.js
+++ src/Paola/Toggle.js
@@ -1,23 +1,25 @@
-import React from "react"
-import {Collapse} from "@chakra-ui/core";
-import styles from "./PaolaStyles.module.scss"
+import React from "react";
+import { Collapse } from "@chakra-ui/core";
+import styles from "./Components/CardStyles.module.scss";
 
-function Toggle(props){
-    const [show, setShow] = React.useState(false);
-    const handleToggle = () => setShow(!show);
-    return (
-        <div className={styles.Tog}>
-            <div className={styles.Buttons}>
-                <button onClick={handleToggle}>Abstract</button>
-                <a className={styles.Article} target="_blank" href={props.link}>Leer artículo</a>
-            </div>
-            <div className={styles.Abstract}>
-                <Collapse mt={4} isOpen={show}>
-                    <p>{props.abs}</p>
-                </Collapse>
-            </div>
-        </div>
-    )
+function Toggle(props) {
+  const [show, setShow] = React.useState(false);
+  const handleToggle = () => setShow(!show);
+  return (
+    <div className={styles.Tog}>
+      <div className={styles.Buttons}>
+        <button onClick={handleToggle}>Abstract</button>
+        <a className={styles.Article} target="_blank" href={props.link}>
+          Leer artículo
+        </a>
+      </div>
+      <div className={styles.Abstract}>
+        <Collapse mt={4} isOpen={show}>
+          <p>{props.abs}</p>
+        </Collapse>
+      </div>
+    </div>
+  );
 }
 
 export default Toggle;
