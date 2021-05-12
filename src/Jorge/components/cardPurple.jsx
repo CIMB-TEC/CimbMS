5350adb35ebafb71d927f343007117389a4183f6
a01658142 <a01658142@itesm.mx>
2021-05-11 20:32:12 -0500

    tarjetas de la parte proyectos

---
+++ src/Jorge/components/cardPurple.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from "./cardPurpleStyles.module.scss"
+import {GiClick} from "react-icons/gi"
+import {GiTechnoHeart} from "react-icons/gi"
+
+function cardPurple(props){
+    return(
+        <div className={styles.container}>
+            <div className={styles.card}>
+                <div className= { `${styles.face} ${styles.face1} `}>
+                    <div className={styles.content}>
+                        <GiTechnoHeart className={styles.icono2} alt=""/>
+                        <h3>{props.Titulo}
+                        </h3>
+                        <div className={styles.arrow}>
+                            <GiClick alt=""/>
+                        </div>
+                    </div>
+                </div>
+                <div className= { `${styles.face} ${styles.face2} `}>
+                    <div className={styles.content}>
+                        <p>{props.Texto}
+                        </p>
+                        <a className={styles.anchor} href={props.enlace}>{props.enlaceTitulo}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+    
+export default cardPurple;
+    
