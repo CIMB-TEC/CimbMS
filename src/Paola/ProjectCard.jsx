20b42b22a6307e9912d7b79465894ef7195e7e5a
paofgz <paofgz2000@gmail.com>
2021-05-18 20:39:09 -0500

    project cards

--- src/Paola/ProjectCard.jsx
+++ src/Paola/ProjectCard.jsx
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import styles from './ProjectCardStyles.module.scss'
 
-const ProjectCard = () => {
+const ProjectCard = (props) => {
 
   return (
     <div>
-      <p>Hola</p>
+      <article className={styles.card}>
+        <img className={styles.thumb} src={props.Img} />
+        <div className={styles.infos}> 
+            <h2 className={styles.title}>{props.Title}</h2>
+            <h3 className={styles.date}>{props.Date}</h3>
+            <h3 className={styles.group}>{props.Group}</h3>
+            <div className={styles.txt}>
+              <h3>Objetivo general</h3>
+              <p>{props.ObjGen}</p>
+              <h3>Objetivo específico</h3>
+              <p>{props.ObjEsp}</p>
+            </div>
+            <a href={props.Link} className={styles.details}> Descargar póster </a>
+        </div>
+      </article>
     </div>
-
   );
 }
 
