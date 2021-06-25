36e5903c15da28081ad323245d98e7956b4ca50e
einar <einarlop0@gmail.com>
2021-06-24 23:06:48 -0500

    Blogdata from firebase

--- src/Firebase/Blog.jsx
+++ src/Firebase/Blog.jsx
@@ -1,15 +1,57 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import styles from "./BlogStyles.module.scss"
+import firebase from "firebase"
+
 
 
 import BlogCard from "./BlogCard.jsx"
 const Blog = () => {
+
+	const [blogs, setBlogs] = useState([])
+
+	const db = firebase.firestore()
+
+	const getValue = async () => {
+		const blogRef = db.collection("blog");
+		const blogsDB = await blogRef.get()
+		if (blogsDB.empty) {
+			console.log("No blogs")
+			return
+		}
+		blogsDB.forEach(blog => {
+			setBlogs(blogs => [...blogs, blog.data()])
+		})
+	}
+
+
+	useEffect(() => {
+		getValue()
+
+	}, [])
+
+
+
 	return (
 		<div className={styles.Wrapper} >
-			<BlogCard />
-			<BlogCard />
+			{blogs.map((blog, index) => {
+				return (
+					<BlogCard
+						key={index}
+						author={blog.author}
+						date={blog.date}
+						description={blog.description}
+						img={blog.img}
+						tag={blog.tag}
+						title={blog.title}
 
-		</div>
+					/>
+
+				);
+
+			})}
+
+
+		</div >
 	)
 
 
